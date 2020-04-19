192200152a5ed4667e50e21a746d3eac6427b138
Ryan Wu <ryanwu646@gmail.com>
2020-04-19 18:27:30 -0400

    New everything to use neovim

---
+++ dotfiles.sh
@@ -0,0 +1,336 @@
+#!/bin/bash
+
+DOTFILES_ROOT="$(dirname $(realpath $0))"
+TMPDIR=
+
+################################################################################
+# Logging
+################################################################################
+NOCOLOR='\033[0m'
+LIGHTRED='\033[1;31m'
+YELLOW='\033[1;33m'
+LIGHTCYAN='\033[1;36m'
+
+log_err() {
+	echo -e "${LIGHTRED}[-] ${@}${NOCOLOR}"
+}
+
+log_wrn() {
+	echo -e "${YELLOW}[!] ${@}${NOCOLOR}"
+}
+
+log_inf() {
+	echo -e "${LIGHTCYAN}[*] ${@}${NOCOLOR}"
+}
+
+################################################################################
+# Funcs
+################################################################################
+
+#
+# utils
+#
+
+symlink() {
+	local src=$1
+	local dst=$2
+	local dst_parent=$(dirname $(realpath $dst))
+
+	if [ ! -d $dst_parent ]
+	then
+		mkdir --parents $dst_parent
+	fi
+
+	if [ ! -f $dst ]
+	then
+		log_inf "creating symlink: $src -> $dst"
+		ln --symbolic $src $dst
+	fi
+}
+
+apt_install() {
+	local pkgs_to_install=
+	for pkg in $@
+	do
+		apt-mark showinstall | grep -q "^$pkg$"
+		if [ $? -ne 0 ]
+		then
+			log_inf "installing package: $pkg"
+			pkgs_to_install+=" $pkg"
+		else
+			log_inf "already installed: $pkg"
+		fi
+	done
+
+	if [ -n "$pkgs_to_install" ]
+	then
+		sudo apt install --yes $pkgs_to_install
+	fi
+}
+
+apt_remove() {
+	local pkgs_to_remove=
+	for pkg in $@
+	do
+		apt-mark showinstall | grep -q "^$pkg$"
+		if [ $? -eq 0 ]
+		then
+			log_inf "removing package: $pkg"
+			pkgs_to_remove+=" $pkg"
+		else
+			log_inf "not installed: $pkg"
+		fi
+	done
+
+	if [ -n "$pkgs_to_remove" ]
+	then
+		sudo apt remove --yes $pkgs_to_remove
+	fi
+}
+
+#
+# bashrc
+#
+
+DOTFILES_HEADER="##### DOTFILES HDR #####"
+DOTFILES_FOOTER="##### DOTFILES FTR #####"
+
+install_bashrc() {
+	log_inf "installing bashrc"
+
+	uninstall_bashrc
+
+	echo "$DOTFILES_HEADER"           >> $HOME/.bashrc
+	cat  $DOTFILES_ROOT/bashrc/bashrc >> $HOME/.bashrc
+	echo "$DOTFILES_FOOTER"           >> $HOME/.bashrc
+}
+
+uninstall_bashrc() {
+	log_inf "uninstalling bashrc"
+
+	if [ -f $HOME/.bashrc ]; then
+		sed -i "/^${DOTFILES_HEADER}\$/,/^${DOTFILES_FOOTER}\$/d" $HOME/.bashrc
+	fi
+}
+
+#
+# FZF
+#
+
+install_fzf() {
+	log_inf "installing fzf"
+
+	local fzf_path="$HOME/.fzf"
+
+	if [ ! -d "$fzf_path" ]; then
+		log_inf "cloning fzf repo"
+		git clone --depth 1 https://github.com/junegunn/fzf.git $fzf_path
+	else
+		log_inf "updating fzf repo"
+		pushd $fzf_path
+		git pull
+		popd
+	fi
+
+	yes | $fzf_path/install
+}
+
+uninstall_fzf() {
+	log_inf "uninstalling fzf"
+
+	local fzf_path="$HOME/.fzf"
+
+	if [ -d "$fzf_path" ]; then
+		$fzf_path/uninstall
+		rm -rf $fzf_path
+	fi
+}
+
+#
+# ripgrep
+#
+
+install_rg() {
+	log_inf "installing rg"
+	apt_install ripgrep
+}
+
+uninstall_rg() {
+	log_inf "uninstalling rg"
+	apt_remove ripgrep
+}
+
+#
+# neovim
+#
+
+install_neovim() {
+	log_inf "installing neovim"
+
+	# install packages
+	log_inf "installing neovim pkgs"
+	apt_install neovim curl xclip exuberant-ctags global python3-pip
+	pip3 install --user pynvim
+
+	# install init.vim
+	log_inf "installing init.vim"
+	symlink $DOTFILES_ROOT/nvim/init.vim $HOME/.config/nvim/init.vim
+
+	# install plugin manager
+	local dein_path="$HOME/.cache/dein"
+	if [ ! -d "$dein_path" ]
+	then
+		log_inf "installing dein.vim"
+		curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > \
+			$TMPDIR/installer.sh
+		sh $TMPDIR/installer.sh $dein_path
+	fi
+}
+
+uninstall_neovim() {
+	log_inf "uninstalling neovim"
+
+	# uninstall packages
+	apt_remove neovim
+	pip3 uninstall --yes pynvim
+
+	# uninstall init.vim
+	if [ -f $HOME/.config/nvim/init.vim ]
+	then
+		log_inf "uninstalling init.vim"
+		rm $HOME/.config/nvim/init.vim
+	fi
+
+	# uninstall plugin manager
+	local dein_path="$HOME/.cache/dein"
+	if [ -d "$dein_path" ]
+	then
+		log_inf "uninstalling dein.vim"
+		rm -rf $dein_path
+	fi
+}
+
+#
+# tmux
+#
+
+install_tmux() {
+	log_inf "installing tmux"
+
+	# install packages
+	log_inf "installing tmux pkgs"
+	apt_install tmux
+
+	# install init.vim
+	log_inf "installing tmux.conf"
+	symlink $DOTFILES_ROOT/tmux/tmux.conf $HOME/.tmux.conf
+
+	# update tmuxline
+	log_inf "updating tmuxline"
+	TMUXLINE_SNAPFILE="~/.tmuxline.snap"
+	tmux new 'vim +"TmuxlineSnapshot! $TMUXLINE_SNAPFILE" +qall'
+}
+
+uninstall_tmux() {
+	log_inf "uninstalling tmux"
+
+	# uninstalling packages
+	apt_remove tmux
+
+	# uninstall init.vim
+	if [ -f $HOME/.tmux.conf ]
+	then
+		log_inf "uninstalling tmux.conf"
+		rm $HOME/.tmux.conf
+	fi
+
+	# uninstall tmuxline
+	if [ -f $HOME/.tmuxline.snap ]
+	then
+		log_inf "uninstalling tmuxline"
+		rm $HOME/.tmuxline.snap
+	fi
+}
+
+################################################################################
+# Main
+################################################################################
+
+install_all() {
+	log_inf "installing all"
+	install_bashrc
+	install_fzf
+	install_rg
+	install_neovim
+	install_tmux
+}
+
+uninstall_all() {
+	log_inf "uninstalling all"
+	uninstall_bashrc
+	uninstall_fzf
+	uninstall_rg
+	uninstall_neovim
+	uninstall_tmux
+}
+
+usage() {
+	log_inf "dotfiles.sh [-hxiu]"
+	log_inf " -h     Print usage"
+	log_inf " -x     Enable verbose debugging"
+	log_inf " -i     Install dotfiles"
+	log_inf " -u     Uninstall dotfiles"
+}
+
+main() {
+	local mode=
+	while getopts ":hiu" opt
+	do
+		case $opt in
+			h)
+				usage
+				return 0
+				;;
+			x)
+				set -x
+				;;
+			i)
+				mode="install"
+				;;
+			u)
+				mode="uninstall"
+				;;
+			\?)
+				log_err "invalid option: $OPTARG"
+				;;
+			:)
+				log_err "invalid option: $OPTARG requires an argument"
+				;;
+		esac
+	done
+	shift $((OPTIND -1))
+
+	if [ -z "$mode" ]
+	then
+		log_err "nothing to do"
+		usage
+		return 1
+	fi
+
+	TMPDIR=$(mktemp --directory --tmpdir=./)
+	log_inf "created tmpdir: $TMPDIR"
+
+	case $mode in
+		install)
+			install_all
+			;;
+		uninstall)
+			uninstall_all
+			;;
+	esac
+
+	rm -rf $TMPDIR
+	log_inf "removed tmpdir: $TMPDIR"
+}
+
+main $@
---
+++ nvim/init.vim
@@ -0,0 +1,214 @@
+" init.vim
+
+" +----------------------------------------------------------------------------+
+" | Plugins
+" +----------------------------------------------------------------------------+
+
+" dein.vim : plugin manager
+if &compatible
+	set nocompatible
+endif
+
+" Add the dein installation directory into runtimepath
+set runtimepath+=~/.cache/dein/repos/github.com/Shougo/dein.vim
+
+if dein#load_state('~/.cache/dein')
+	call dein#begin('~/.cache/dein')
+
+	call dein#add('~/.cache/dein/repos/github.com/Shougo/dein.vim')
+	call dein#add('Shougo/deoplete.nvim')
+	if !has('nvim')
+		call dein#add('roxma/nvim-yarp')
+		call dein#add('roxma/vim-hug-neovim-rpc')
+	endif
+
+	" colorscheme
+	call dein#add('w0ng/vim-hybrid')
+
+	" statusbar
+	call dein#add('vim-airline/vim-airline')
+	call dein#add('vim-airline/vim-airline-themes')
+	call dein#add('powerline/fonts')
+
+	" git
+	call dein#add('tpope/vim-fugitive')
+	call dein#add('airblade/vim-gitgutter')
+
+	" fzf
+	set runtimepath+=~/.fzf
+	call dein#add('junegunn/fzf.vim')
+
+	" tmux
+	call dein#add('christoomey/vim-tmux-navigator')
+	call dein#add('edkolev/tmuxline.vim')
+
+	" window movement
+	call dein#add('wesQ3/vim-windowswap')
+
+	" tags
+	call dein#add('ludovicchabant/vim-gutentags')
+	call dein#add('skywind3000/gutentags_plus')
+
+	" quality of life
+	call dein#add('tpope/vim-commentary')
+	call dein#add('tpope/vim-eunuch')
+	call dein#add('tpope/vim-surround')
+	call dein#add('tpope/vim-repeat')
+
+	call dein#end()
+	call dein#save_state()
+endif
+
+filetype plugin indent on
+syntax enable
+
+" Install new plugins on startup
+if dein#check_install()
+	call dein#install()
+endif
+
+" +----------------------------------------------------------------------------+
+" | Plugin Options
+" +----------------------------------------------------------------------------+
+
+" deoplete
+let g:deoplete#enable_at_startup = 1
+
+" vim-airline
+let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 1
+
+" windowsnap
+let g:windowswap_map_keys = 0 "prevent default bindings
+
+" gutentags
+let g:gutentags_modules = ['ctags', 'gtags_cscope']
+let g:gutentags_project_root = ['.root']
+let g:gutentags_cache_dir = '~/.cache/tags'
+let g:gutentags_plus_switch = 1
+
+" +----------------------------------------------------------------------------+
+" | Remaps
+" +----------------------------------------------------------------------------+
+
+" Make Vim recognize XTerm escape sequences for Page and Arrow
+" keys combined with modifiers such as Shift, Control, and Alt.
+" See http://www.reddit.com/r/vim/comments/1a29vk/_/c8tze8p
+if &term =~ '^screen'
+	" Page keys http://sourceforge.net/p/tmux/tmux-code/ci/master/tree/FAQ
+	execute "set t_kP=\e[5;*~"
+	execute "set t_kN=\e[6;*~"
+
+	" Arrow keys http://unix.stackexchange.com/a/34723
+	execute "set <xUp>=\e[1;*A"
+	execute "set <xDown>=\e[1;*B"
+	execute "set <xRight>=\e[1;*C"
+	execute "set <xLeft>=\e[1;*D"
+endif
+
+" Set leader key
+let mapleader = " "
+
+" Clear hlsearch
+nnoremap <C-c> :nohlsearch<CR>
+
+" Windows
+nnoremap <C-h> <C-w>h
+nnoremap <C-j> <C-w>j
+nnoremap <C-k> <C-w>k
+nnoremap <C-l> <C-w>l
+
+nnoremap <Leader>s <C-w>s
+nnoremap <Leader>v <C-w>v
+nnoremap <Leader>= <C-w>=
+nnoremap <leader>w :call WindowSwap#EasyWindowSwap()<CR>
+
+nnoremap <Leader>n :bnext<CR>
+nnoremap <Leader>p :bprevious<CR>
+nnoremap <Leader>l :buffer #<CR>
+nnoremap <Leader>d :bdelete<CR>
+
+" fugitive
+nnoremap <Leader>gb :Gblame<CR>
+nnoremap <Leader>gd :Gdiff<CR>
+nnoremap <Leader>gs :Gstatus<CR>
+
+" fzf
+nnoremap <Leader>f :Files<CR>
+nnoremap <Leader>b :Buffers<CR>
+nnoremap <Leader>r :Rg<CR>
+nnoremap <Leader>g :Tags<CR>
+
+" tab completion
+inoremap <expr> <tab> pumvisible() ? "\<C-n>" : "\<tab>"
+inoremap <expr> <C-j> pumvisible() ? "\<C-n>" : "\<C-j>"
+inoremap <expr> <C-k> pumvisible() ? "\<C-p>" : "\<C-k>"
+
+" terminal
+if has('nvim')
+	" New terminal
+	noremap <Leader>t :terminal<CR>
+
+	" <esc> to return to normal mode
+	tnoremap <esc> <C-\><C-N>
+
+	" Start terminal without line numbers
+	autocmd TermOpen * setlocal nonumber norelativenumber
+endif
+
+" +----------------------------------------------------------------------------+
+" | Appearance
+" +----------------------------------------------------------------------------+
+
+" Colorscheme
+colorscheme hybrid
+
+" Set primary text to white
+highlight Normal ctermfg=White
+
+" Set background to terminal
+highlight Normal ctermbg=None
+
+" Set special char to blue
+highlight SpecialChar ctermfg=167 guifg=#cc6666
+
+" vim-airline theme
+let g:airline_theme = 'wombat'
+
+" Show cursor line
+set cursorline
+
+" Line numbers
+set number
+
+" 80 char limit
+set colorcolumn=81
+
+" Tab width
+set shiftwidth=4
+set tabstop=4
+
+" +----------------------------------------------------------------------------+
+" | Miscellaneous
+" +----------------------------------------------------------------------------+
+
+" Enable mouse support
+set mouse=a
+
+" Allow modified buffers to be hidden
+set hidden
+
+" Split behavior
+set splitright
+set splitbelow
+
+" Search options
+nmap / /\v
+nmap ? ?\v
+
+" Highlight whitespace
+highlight ExtraWhitespace ctermbg=red guibg=red
+match ExtraWhitespace /\s\+$/
+
+" Copy/paste from the system clipboard
+set clipboard=unnamedplus
